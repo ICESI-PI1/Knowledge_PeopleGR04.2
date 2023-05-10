5be978eb6a814057dc880dbba7be4d2d1d9a5cac
CarlosJaBr <carjabo12@gmail.com>
2023-05-09 21:36:57 -0500

    fix: error has been fixed

--- users/tests.py
+++ users/tests.py
@@ -8,11 +8,24 @@ class UsersManagersTests(TestCase):
     def test_create_user(self):
         user = User.objects.create_user(email="normal@user.com", password="foo", name="little user", idType=1,numID=123456789,role=1)
         self.assertEqual(user.email, "normal@user.com")
+        user.email = "carjabo12@gmail.com"
+        self.assertEqual(user.email, "carjabo12@gmail.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
+    
+    def test_noncreate_user(self):    
+        try:
+            user = User.objects.create_user(email="normal12@user.com", password="foo12", name="", idType=1,numID=12345678999,role=1)
+            #self.assertFalse(user.is_active)
+        except: 
+            self.assertFalse(user.is_active)
+    
+    
 
-        '''
+
+
+    '''
         try:
             # username is None for the AbstractUser option
             # username does not exist for the AbstractBaseUser option
