4c3c62e4cb1fa753dfb18d7292fe04a4e3676e29
Carlos V <villegas.carlos043@gmail.com>
2023-05-12 00:30:17 -0500

    style_add_new_edit_template

--- scholarships/urls.py
+++ scholarships/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [path("showmenu/", views.ShowMenu.as_view(), name="showmenu"),
                 path('naturaldonor/<int:pk>/update/', views.NaturalDonorUpdateView.as_view(), name='natural_donor_profile_update'),
                 path('legaldonor/<int:pk>/update/', views.LegalDonorUpdateView.as_view(), name='legal_donor_profile_update'),
                 path('institution/<int:pk>/update/', views.InstitutionUpdateView.as_view(), name='institution_profile_update'),
+                path("soli_Activa/", views.ActiveSolicitud.as_view(), name="soli_Activa"),
             ]
